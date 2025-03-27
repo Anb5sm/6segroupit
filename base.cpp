ae8bba2d4b597c3b571532872a9f223ed710834b
Anb5sm <bardhananuj@gmail.com>
2025-03-28 01:34:18 +0530

    commites

---
+++ base.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;  
+
+    int a = 5, b = 10;  
+    int sum = a + b;    
+
+    std::cout << "Sum: " << sum << std::endl;  
+
+    return 0;  
+}
