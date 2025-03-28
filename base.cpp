0981a4c1332f7f33a4ec01cf7d4e0eee034a723f
Anb5sm <bardhananuj@gmail.com>
2025-03-28 07:50:50 +0530

    update

--- base.cpp
+++ base.cpp
@@ -4,8 +4,8 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;  
 
-    int a = 10, b = 16;  // Proper variable declaration
-    int sum = a + b;    
+    int a = 10, b = 16 ,d = 10; 
+    int sum = a + b + d;    
 
     std::cout << "Sum: " << sum << std::endl;  
 
