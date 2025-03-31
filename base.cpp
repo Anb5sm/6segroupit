874e2c3bbcf3862e0ba0a1761effbec4aea1eb6d
Anb5sm <bardhananuj@gmail.com>
2025-04-01 00:05:16 +0530

    upd

--- base.cpp
+++ base.cpp
@@ -4,8 +4,8 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;  
 
-    int a = 10, b = 16 ,d = 10; 
-    int sum = a + b + d;    
+    int a = 10, b = 16 , f = 1 ; 
+    int sum = a + b + f ;    
 
     std::cout << "Sum: " << sum << std::endl;  
 
